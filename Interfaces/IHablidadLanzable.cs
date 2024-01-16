f5d4cfb9c86c7b8a88ee7cba04671d61f390de27
Alan-2023Dts <150711078+Alan-2023Dts@users.noreply.github.com>
2024-01-16 08:24:44 -0600

    Se crean dos interfaces nuevas para la funcion de lanzar habilidades
    Se creo una nueva rama para seguridad del codigo, en este cambio se agregaron nuevas interfaces para crear personajes por medio de heredacion y otra donde se escribe el constructor para lanzar habilidades

---
+++ Interfaces/IHablidadLanzable.cs
@@ -0,0 +1,26 @@
+﻿using Pruebas_35.Interfaces.Clases;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pruebas_35.Interfaces
+{
+
+
+    public enum TipoHabilidadLanzable
+    {
+        Dano,
+        Curacion
+    }
+    public interface IHabilidadLanzable : IHabilidad
+    {
+        int CostoMana { get; }
+        bool AutoAplicable { get; }
+        TipoHabilidadLanzable Tipo { get; }
+        int Potencia { get; }
+
+        void Lanzar(IPersonaje lanzador, IPersonaje objetivo);
+    }
+}
