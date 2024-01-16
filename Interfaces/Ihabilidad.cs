eac69ef4d8c817b40f4c48ac7fd1320ffdb93e3a
Alan-2023Dts <150711078+Alan-2023Dts@users.noreply.github.com>
2024-01-16 07:49:13 -0600

    Se agrega El enumerador de tipo de habilidad
    Se implemento un enumerador para definir el tipo de habilidad

--- Interfaces/Ihabilidad.cs
+++ Interfaces/Ihabilidad.cs
@@ -6,14 +6,22 @@ using System.Threading.Tasks;
 
 namespace Pruebas_35.Interfaces.Clases
 {
-  
 
+    public enum TipoHabilidad
+    {
+        Activa,
+        Pasiva
+    }
+
+   
     public interface IHabilidad
     {
         string Id { get; }
         string Nombre { get; }
         int NivelRequerido { get; }
-     
+
+        TipoHabilidad Clase { get; }
+
     }
 
 }
